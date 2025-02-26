b9586379acb6f57b6aae2bda17aa0176ba3352c6
Rivemn <132005694+Rivemn@users.noreply.github.com>
2025-02-26 23:07:12 +0200

    Added git.gnore

--- BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
+++ BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16df83e9f48d295f9f7c8670ad56aeff87215f78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dbe6f617ebac3d1f79fce96072985867030ac78")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
