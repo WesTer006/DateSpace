bb72268113b6406e98c674841119e914bcc031a0
WesTer006 <162494366+WesTer006@users.noreply.github.com>
2025-03-07 12:37:14 +0200

    Feat/4/add swagger (#4)
    * Added all necessary packages
    
    * added swagger with xml-comments
    
    * improved swagger
    
    * deleted unnecessary nuget packages OpenApi

---
+++ DateSpaceWebAPI/Extensions/SwaggerConfig.cs
@@ -0,0 +1,79 @@
+﻿using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+namespace DateSpaceWebAPI.Extensions
+{
+    /// <summary>
+    /// Клас для налаштування Swagger в додатку.
+    /// </summary>
+    public static class SwaggerConfig
+    {
+        /// <summary>
+        /// Додає налаштування Swagger до контейнера служб.
+        /// </summary>
+        /// <param name="services">Інстанс контейнера служб</param>
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "DateSpace WebAPI",
+                    Version = "v1",
+                    Description = "API для дейтінг-додатка"
+                });
+
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Введіть токен в форматі: Bearer {твій_токен}"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+        }
+
+        /// <summary>
+        /// Налаштовує Swagger для додатку.
+        /// </summary>
+        /// <param name="app">Інстанс веб-додатку</param>
+        public static void UseSwaggerConfiguration(this WebApplication app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DateSpace API v1");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/")
+                {
+                    context.Response.Redirect("/swagger");
+                    return;
+                }
+                await next();
+            });
+        }
+    }
+}
--- DateSpaceWebAPI/Program.cs
+++ DateSpaceWebAPI/Program.cs
@@ -1,17 +1,16 @@
+using DateSpaceWebAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddSwaggerConfiguration();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwaggerConfiguration();
 }
 
 app.UseHttpsRedirection();
@@ -20,4 +19,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
