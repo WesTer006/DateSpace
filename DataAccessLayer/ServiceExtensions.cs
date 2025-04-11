83b1227d0ff600ef6e2e1bd36fc9dd4a03ab2b7a
Rivemn <132005694+Rivemn@users.noreply.github.com>
2025-04-11 23:27:46 +0300

    migrate db to cloud

--- DataAccessLayer/ServiceExtensions.cs
+++ DataAccessLayer/ServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Data;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using DataAccessLayer.Data;
 using DataAccessLayer.Interfaces;
 using DataAccessLayer.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -12,16 +14,39 @@ namespace DataAccessLayer
 		public static IServiceCollection AddDataAccessDependencies(this IServiceCollection services,
 	    IConfiguration configuration)
 		{
+			var connectionString = GetConnectionStringFromAzureKeyVault(configuration);
+
 			return services
-				.AddCustomDbContext(
-				configuration.GetConnectionString("SqlServerConnection"))
+				.AddCustomDbContext(connectionString)
                 .AddScoped<IUnitOfWork, UnitOfWork>()
-				 .AddScoped<IUserRepository, UserRepository>();
+				.AddScoped<IUserRepository, UserRepository>();
 		}
 		private static IServiceCollection AddCustomDbContext(this IServiceCollection services, string? connectionString)
 		{
 			return services.AddDbContext<AppDbContext>(options =>
 				options.UseSqlServer(connectionString, x => x.UseNetTopologySuite()));
+		}
+
+		private static string GetConnectionStringFromAzureKeyVault(IConfiguration configuration)
+		{
+			var secretUrl = configuration["ConnectionStrings:SecretSqlServerConnection"];
+			if (string.IsNullOrEmpty(secretUrl))
+				throw new InvalidOperationException("SecretSqlServerConnection URL is missing from configuration.");
+
+
+			var uri = new Uri(secretUrl);
+			var vaultBaseUrl = $"{uri.Scheme}://{uri.Host}";
+			var segments = uri.Segments;
+			if (segments.Length < 3)
+				throw new FormatException("Invalid Key Vault secret URL format. Expected format: https://<vault>/secrets/<name>/<version>");
+
+			var secretName = segments[2].TrimEnd('/');
+
+			
+			var client = new SecretClient(new Uri(vaultBaseUrl), new DefaultAzureCredential());
+			var secret = client.GetSecret(secretName);
+
+			return secret.Value.Value;
 		}
 	}
 }
