a7842945427f69189c2a9ca5cc349ad770c0697d
WesTer <romanvs0602@gmail.com>
2025-04-28 16:27:00 +0300

    updated database connection

--- DateSpaceWebAPI/Program.cs
+++ DateSpaceWebAPI/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.CookiePolicy;
 
 var builder = WebApplication.CreateBuilder(args);
 
-bool useLocalDb = args.Contains("UseLocalDb", StringComparer.OrdinalIgnoreCase);
+bool useLocalDb = builder.Configuration.GetValue<bool>("UseLocalDb");
 
 builder.Services.AddSwaggerConfiguration();
 
